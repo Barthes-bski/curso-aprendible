826a8187d77dde64614bb3bb107bb84ae073a408
Matias Soto <matias.delfos.l@gmail.com>
2024-01-08 23:43:17 -0300

    First

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Chirp;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', [\App\Http\Controllers\ChirpController::class, 'index'])->name('chirps.index');
+
+    Route::post('/chirps',[\App\Http\Controllers\ChirpController::class, 'store'])
+        ->name('chirps.store');
+
+    Route::get('/chirps/{chirp}/edit',[\App\Http\Controllers\ChirpController::class, 'edit'])
+        ->name('chirps.edit');
+
+    Route::put('/chirps/{chirp}',[\App\Http\Controllers\ChirpController::class, 'update'])
+        ->name('chirps.update');
+
+    Route::delete('/chirps/{chirp}',[\App\Http\Controllers\ChirpController::class, 'destroy'])
+        ->name('chirps.destroy');
+});
+
+require __DIR__.'/auth.php';
